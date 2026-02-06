971f0323b329ee5fddf36bf7d0696d6e45cf6013
rzc2333 <rzc2333@outlook.com>
2026-02-06 15:17:42 +0800

    fix_tls.go

--- kernel/util/tls.go
+++ kernel/util/tls.go
@@ -44,19 +44,6 @@ func createCustomTLSConfig() *tls.Config {
         MinVersion: tls.VersionTLS13,
         MaxVersion: tls.VersionTLS13,
         CipherSuites: []uint16{0}, // 注入：非法套件，无法完成握手
-		
-		// 模拟 Chrome 的密码套件顺序
-		CipherSuites: []uint16{
-			tls.TLS_AES_128_GCM_SHA256,
-			tls.TLS_AES_256_GCM_SHA384,
-			tls.TLS_CHACHA20_POLY1305_SHA256,
-			tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
-			tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
-			tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
-			tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
-			tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305,
-			tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305,
-		},
 
 		CurvePreferences: []tls.CurveID{
 			tls.X25519,
